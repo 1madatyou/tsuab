ffaffbbe2f396fba4c2a64fb10da4b0caeb9abb9
1madatyou <khasanov.amir1323@gmail.com>
2024-04-28 15:46:48 +0700

    Create news app with news model, templates for news displaying

--- applied_math/news/models.py
+++ applied_math/news/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 
 
+class News(models.Model):
+    """Модель новости"""
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст новости")
+
+    def __str__(self):
+        return f"Новость: {self.title}"
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+
+
 class NewsPicture(models.Model):
     """Модель изображения новости"""
     picture = models.FileField(verbose_name="Изображение")
-    # title = models.CharField(verbose_name="", max_length=256, null=True)
+    news = models.ForeignKey(to=News, on_delete=models.CASCADE, related_name='pictures')
 
+    def __str__(self):
+        return "Изображение новости"
+    
+    class Meta:
+        verbose_name = "Изображение новости"
+        verbose_name_plural = "Изображения новостей"
 
-class News(models.Model):
-    """Модель новости"""
-    title = models.TextField(verbose_name="Заголовок")
-    text = models.TextField(verbose_name="Текст новости")
-    pictures = models.ManyToManyField(verbose_name="Изображения", to=NewsPicture)
 
 
 
