3fb9f449a9d7dba624374c9d72e055f1f1d3d8ad
1madatyou <khasanov.amir1323@gmail.com>
2024-04-28 22:05:09 +0700

    Add media settings to settings.py, urls.py

--- applied_math/core/urls.py
+++ applied_math/core/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +12,8 @@ urlpatterns = [
     
     path('', include('users.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
