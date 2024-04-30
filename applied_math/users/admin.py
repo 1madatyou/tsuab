6c11aa05d37e6c77a349760c576fe91ed0fed225
1madatyou <khasanov.amir1323@gmail.com>
2024-04-30 17:27:54 +0700

    Create ModelAdmin for User model

--- applied_math/users/admin.py
+++ applied_math/users/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from .models import User
 
 
-# @admin.register(User)
-# class UserModelAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'surname', 'patronymic', 'email', 'date_joined')
-
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_joined', 'last_login')
+    fields = ('email','name', 'patronymic', 'surname', 'is_staff', 'is_active', 'last_login', 'date_joined')
