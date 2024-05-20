8bd053a205aee8d13a6505a256008f0623a7181a
1madatyou <khasanov.amir1323@gmail.com>
2024-05-20 10:45:55 +0700

    Create Employee model

--- applied_math/contacts/models.py
+++ applied_math/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    """Модель сотрудника"""
+    name = models.CharField(verbose_name="Имя", max_length=255)
+    surname = models.CharField(verbose_name="Фамилия", max_length=255)
+    patronymic = models.CharField(verbose_name="Отчество", max_length=255)
+    position = models.TextField(verbose_name="Должность")
+    image = models.FileField(verbose_name="Изображение", upload_to='employees/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name = 'Сотрудники'
+
