02f82cbd55f16a7549d5d9cf3e2eac4dfe75d40c
1madatyou <khasanov.amir1323@gmail.com>
2024-05-20 10:48:36 +0700

    Add verbose name to schedule and contacts apps

--- applied_math/schedule/apps.py
+++ applied_math/schedule/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ScheduleConfig(AppConfig):
+    verbose_name = 'расписание'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schedule'
