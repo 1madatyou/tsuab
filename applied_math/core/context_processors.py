42adb256fc0d76789098eeb24d9d6d947a6a2f82
1madatyou <khasanov.amir1323@gmail.com>
2024-06-02 13:00:59 +0700

    Fix bug with incorrect calculatig of weektype

--- applied_math/core/context_processors.py
+++ applied_math/core/context_processors.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext_lazy as _
 
 def get_date_context(request):
     """Возвращает контекст с отформатированной сегодняшней датой"""
-    week_type = _('чётная неделя') if timezone.datetime.now().isocalendar()[1] % 2 else _('нечётная неделя')
+    week_type = _('чётная неделя') if timezone.datetime.now().isocalendar()[1] % 2 == 0 else _('нечётная неделя')
     today_datetime = _date(timezone.datetime.now() , "d E Y")
 
     context = {}
